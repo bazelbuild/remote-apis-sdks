1b78566cf6c1b3d6c9e5e335c26c3411bfdf9659
Ola Rozenfeld <olaola@google.com>
2019-07-11 15:34:10 -0400

    A Chunker to support memory-bounded buffered uploads with lazy file (#26)
    * A Chunker to support memory-bounded buffered uploads with lazy file
    reads.
    
    * Addressing offline comments
    
    * Addressing comments
    
    * Making the Digest public instead of just the Size
    
    * Reconsidered, I don't need to have Digest as part of Chunk.
    
    * Adding the post-Next-reliability contraint into the unit tests. Enabling more tests.
    
    * Stupid Go won't print an int into a %s...

---
+++ go/pkg/chunker/chunker.go
@@ -0,0 +1,183 @@
+// Package chunker provides a way to chunk an input into uploadable-size byte slices.
+package chunker
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/digest"
+)
+
+// DefaultChunkSize is the default chunk size for ByteStream.Write RPCs.
+const DefaultChunkSize = 1024 * 1024
+
+// IOBufferSize regulates how many bytes at a time the Chunker will read from a file source.
+var IOBufferSize = 10 * 1024 * 1024
+
+// ErrEOF is returned when Next is called when HasNext is false.
+var ErrEOF = errors.New("ErrEOF")
+
+// Chunker can be used to chunk an input into uploadable-size byte slices.
+// A single Chunker is NOT thread-safe; it should be used by a single uploader thread.
+type Chunker struct {
+	chunkSize int
+	reader    *bufio.Reader
+	// An optional cache of the full data. It will be present in these cases:
+	// * The Chunker was initialized from a []byte.
+	// * Chunker.FullData was called at least once.
+	// * Next() was called and the read was less than IOBufferSize.
+	// Once contents are initialized, they are immutable.
+	contents    []byte
+	digest      digest.Digest
+	offset      int64
+	initialized bool
+	path        string
+}
+
+// NewFromBlob initializes a Chunker from the provided bytes buffer.
+func NewFromBlob(blob []byte, chunkSize int) *Chunker {
+	if chunkSize < 1 {
+		chunkSize = DefaultChunkSize
+	}
+	return &Chunker{
+		contents:  blob,
+		chunkSize: chunkSize,
+		digest:    digest.NewFromBlob(blob),
+	}
+}
+
+// NewFromFile initializes a Chunker from the provided file.
+// The provided Digest has to match the contents of the file! If the size of the actual contents is
+// shorter than the provided Digest size, the Chunker will error on Next(), but otherwise the
+// results are unspecified.
+func NewFromFile(path string, dg digest.Digest, chunkSize int) *Chunker {
+	if chunkSize < 1 {
+		chunkSize = DefaultChunkSize
+	}
+	if chunkSize > IOBufferSize {
+		chunkSize = IOBufferSize
+	}
+	return &Chunker{
+		chunkSize: chunkSize,
+		digest:    dg,
+		path:      path,
+	}
+}
+
+// String returns an identifiable representation of the Chunker.
+func (c *Chunker) String() string {
+	size := fmt.Sprintf("<%d bytes>", c.digest.Size)
+	if c.path == "" {
+		return size
+	}
+	return fmt.Sprintf("%s: %s", size, c.path)
+}
+
+// Digest returns the digest of the full data of this chunker.
+func (c *Chunker) Digest() digest.Digest {
+	return c.digest
+}
+
+// Offset returns the current Chunker offset.
+func (c *Chunker) Offset() int64 {
+	return c.offset
+}
+
+func (c *Chunker) bytesLeft() int64 {
+	return c.digest.Size - c.offset
+}
+
+// ChunkSize returns the maximum size of each chunk.
+func (c *Chunker) ChunkSize() int {
+	return c.chunkSize
+}
+
+// Reset the Chunker state to when it was newly constructed.
+// Useful for upload retries.
+// TODO(olaola): implement Seek(offset) when we have resumable uploads.
+func (c *Chunker) Reset() {
+	c.reader = nil
+	c.offset = 0
+	c.initialized = false
+}
+
+// FullData returns the overall (non-chunked) underlying data. The Chunker is Reset.
+// It is supposed to be used for batch uploading small inputs.
+func (c *Chunker) FullData() ([]byte, error) {
+	c.Reset()
+	if c.contents != nil {
+		return c.contents, nil
+	}
+	var err error
+	// Cache contents so that the next call to FullData() doesn't result in file read.
+	c.contents, err = ioutil.ReadFile(c.path)
+	return c.contents, err
+}
+
+// HasNext returns whether a subsequent call to Next will return a valid chunk. Always true for a
+// newly created Chunker.
+func (c *Chunker) HasNext() bool {
+	return !c.initialized || c.bytesLeft() > 0
+}
+
+// Chunk is a piece of a byte[] blob suitable for being uploaded.
+type Chunk struct {
+	Offset int64
+	Data   []byte
+}
+
+// Next returns the next chunk of data or error. ErrEOF is returned if and only if HasNext is false.
+// Chunk.Data will be empty if and only if the full underlying data is empty (in which case it will
+// be the only chunk returned). Chunk.Digest will only be filled for the first chunk.
+func (c *Chunker) Next() (*Chunk, error) {
+	if !c.HasNext() {
+		return nil, ErrEOF
+	}
+	c.initialized = true
+	if c.digest.Size == 0 {
+		return &Chunk{}, nil
+	}
+	// Empty contents means we don't have the full data cache, and must read it from a file.
+	if c.contents == nil && c.reader == nil { // Need to initialize the reader.
+		f, err := os.Open(c.path)
+		if err != nil {
+			return nil, err
+		}
+		c.reader = bufio.NewReaderSize(f, IOBufferSize)
+	}
+	bytesLeft := c.bytesLeft()
+	bytesToSend := c.chunkSize
+	if bytesLeft < int64(bytesToSend) {
+		bytesToSend = int(bytesLeft)
+	}
+	var data []byte
+	if c.contents == nil { // Data is being read from a file rather than static contents cache.
+		if c.offset == 0 && c.digest.Size <= int64(IOBufferSize) {
+			data = make([]byte, c.digest.Size)
+			c.contents = data // Cache the contents to avoid Reads on future Resets.
+		} else {
+			data = make([]byte, bytesToSend)
+		}
+		n, err := io.ReadFull(c.reader, data)
+		if err != nil {
+			return nil, err
+		}
+		if n < bytesToSend {
+			return nil, fmt.Errorf("only read %d bytes from %s, expected %d", n, c.path, bytesToSend)
+		}
+	} else {
+		// Data is being read from the cache. Contents are immutable so it's okay to return a slice.
+		data = c.contents[c.offset : int(c.offset)+bytesToSend]
+	}
+	res := &Chunk{
+		Offset: c.offset,
+		// Reading only up to bytesToSend in case contents contains the entire data.
+		Data: data[:bytesToSend],
+	}
+	c.offset += int64(bytesToSend)
+	return res, nil
+}
---
+++ go/pkg/chunker/chunker_test.go
@@ -0,0 +1,375 @@
+package chunker
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/digest"
+	"github.com/google/go-cmp/cmp"
+)
+
+var tests = []struct {
+	name       string
+	blob       []byte
+	wantChunks []*Chunk
+	chunkSize  int
+}{
+	{
+		name:       "empty",
+		wantChunks: []*Chunk{&Chunk{}},
+		chunkSize:  3,
+	},
+	{
+		name:       "one",
+		blob:       []byte("12"),
+		wantChunks: []*Chunk{&Chunk{Data: []byte("12")}},
+		chunkSize:  3,
+	},
+	{
+		name:       "one-even",
+		blob:       []byte("123"),
+		wantChunks: []*Chunk{&Chunk{Data: []byte("123")}},
+		chunkSize:  3,
+	},
+	{
+		name: "two",
+		blob: []byte("12345"),
+		wantChunks: []*Chunk{
+			&Chunk{Data: []byte("123")},
+			&Chunk{Data: []byte("45"), Offset: 3},
+		},
+		chunkSize: 3,
+	},
+	{
+		name: "three-even",
+		blob: []byte("123456789"),
+		wantChunks: []*Chunk{
+			&Chunk{Data: []byte("123")},
+			&Chunk{Data: []byte("456"), Offset: 3},
+			&Chunk{Data: []byte("789"), Offset: 6},
+		},
+		chunkSize: 3,
+	},
+	{
+		name: "three",
+		blob: []byte("123456789"),
+		wantChunks: []*Chunk{
+			&Chunk{Data: []byte("1234")},
+			&Chunk{Data: []byte("5678"), Offset: 4},
+			&Chunk{Data: []byte("9"), Offset: 8},
+		},
+		chunkSize: 4,
+	},
+	{
+		name: "many",
+		blob: []byte("1234567890abcdefghijklmnopqrstuvwxyz!"),
+		wantChunks: []*Chunk{
+			&Chunk{Data: []byte("1234")},
+			&Chunk{Data: []byte("5678"), Offset: 4},
+			&Chunk{Data: []byte("90ab"), Offset: 8},
+			&Chunk{Data: []byte("cdef"), Offset: 12},
+			&Chunk{Data: []byte("ghij"), Offset: 16},
+			&Chunk{Data: []byte("klmn"), Offset: 20},
+			&Chunk{Data: []byte("opqr"), Offset: 24},
+			&Chunk{Data: []byte("stuv"), Offset: 28},
+			&Chunk{Data: []byte("wxyz"), Offset: 32},
+			&Chunk{Data: []byte("!"), Offset: 36},
+		},
+		chunkSize: 4,
+	},
+}
+
+var bufferSizes = []int{3, 4, 8, 100}
+
+func TestChunkerFromBlob(t *testing.T) {
+	t.Parallel()
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			c := NewFromBlob(tc.blob, tc.chunkSize)
+			var gotChunks []*Chunk
+			for _, wantChunk := range tc.wantChunks {
+				if !c.HasNext() {
+					t.Errorf("%s: c.HasNext() was false on blob %q , expecting next chunk %q", tc.name, tc.blob, string(wantChunk.Data))
+				}
+				got, err := c.Next()
+				if err != nil {
+					t.Errorf("%s: c.Next() gave error %v on blob %q , expecting next chunk %q", tc.name, err, tc.blob, string(wantChunk.Data))
+				}
+				gotChunks = append(gotChunks, got)
+			}
+			if diff := cmp.Diff(tc.wantChunks, gotChunks); diff != "" {
+				t.Errorf("%s: Chunker gave result diff (-want +got):\n%s", tc.name, diff)
+			}
+		})
+	}
+}
+
+func TestChunkerFromFile(t *testing.T) {
+	execRoot, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("failed to make temp dir: %v", err)
+	}
+	defer os.RemoveAll(execRoot)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			path := filepath.Join(execRoot, tc.name)
+			if err := ioutil.WriteFile(path, tc.blob, 0777); err != nil {
+				t.Fatalf("failed to write temp file: %v", err)
+			}
+			for _, bufSize := range bufferSizes {
+				if bufSize < tc.chunkSize {
+					continue
+				}
+				dg := digest.NewFromBlob(tc.blob)
+				IOBufferSize = bufSize
+				c := NewFromFile(path, dg, tc.chunkSize)
+				var gotChunks []*Chunk
+				for _, wantChunk := range tc.wantChunks {
+					if !c.HasNext() {
+						t.Errorf("%s: c.HasNext() was false on blob %q buffer size %d, expecting next chunk %q", tc.name, tc.blob, bufSize, string(wantChunk.Data))
+					}
+					got, err := c.Next()
+					if err != nil {
+						t.Errorf("%s: c.Next() gave error %v on blob %q buffer size %d, expecting next chunk %q", tc.name, err, tc.blob, bufSize, string(wantChunk.Data))
+					}
+					gotChunks = append(gotChunks, got)
+				}
+				if diff := cmp.Diff(tc.wantChunks, gotChunks); diff != "" {
+					t.Errorf("%s: Chunker buffer size %d gave result diff (-want +got):\n%s", tc.name, bufSize, diff)
+				}
+			}
+		})
+	}
+}
+
+func TestChunkerFromBlob_Reset(t *testing.T) {
+	t.Parallel()
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			for reset := 1; reset < len(tc.wantChunks); reset++ {
+				c := NewFromBlob(tc.blob, tc.chunkSize)
+				var gotChunks []*Chunk
+				for i, wantChunk := range tc.wantChunks {
+					if !c.HasNext() {
+						t.Errorf("%s: c.HasNext() was false on blob %q , expecting next chunk %q", tc.name, tc.blob, string(wantChunk.Data))
+					}
+					got, err := c.Next()
+					if err != nil {
+						t.Errorf("%s: c.Next() gave error %v on blob %q , expecting next chunk %q", tc.name, err, tc.blob, string(wantChunk.Data))
+					}
+					gotChunks = append(gotChunks, got)
+					if i == reset {
+						c.Reset()
+						break
+					}
+				}
+				if diff := cmp.Diff(tc.wantChunks[:len(gotChunks)], gotChunks); diff != "" {
+					t.Errorf("%s: Chunker gave result diff (-want +got):\n%s", tc.name, diff)
+				}
+				gotChunks = nil
+				if reset >= len(tc.wantChunks) {
+					continue
+				}
+				for _, wantChunk := range tc.wantChunks {
+					if !c.HasNext() {
+						t.Errorf("%s: c.HasNext() was false on blob %q , expecting next chunk %q", tc.name, tc.blob, string(wantChunk.Data))
+					}
+					got, err := c.Next()
+					if err != nil {
+						t.Errorf("%s: c.Next() gave error %v on blob %q , expecting next chunk %q", tc.name, err, tc.blob, string(wantChunk.Data))
+					}
+					gotChunks = append(gotChunks, got)
+				}
+				if diff := cmp.Diff(tc.wantChunks, gotChunks); diff != "" {
+					t.Errorf("%s: Chunker gave result diff (-want +got):\n%s", tc.name, diff)
+				}
+			}
+		})
+	}
+}
+
+func TestChunkerFromFile_Reset(t *testing.T) {
+	execRoot, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("failed to make temp dir: %v", err)
+	}
+	defer os.RemoveAll(execRoot)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			path := filepath.Join(execRoot, tc.name)
+			if err := ioutil.WriteFile(path, tc.blob, 0777); err != nil {
+				t.Fatalf("failed to write temp file: %v", err)
+			}
+			for _, bufSize := range bufferSizes {
+				if bufSize < tc.chunkSize {
+					continue
+				}
+				dg := digest.NewFromBlob(tc.blob)
+				IOBufferSize = bufSize
+				for reset := 1; reset < len(tc.wantChunks); reset++ {
+					c := NewFromFile(path, dg, tc.chunkSize)
+					var gotChunks []*Chunk
+					for i, wantChunk := range tc.wantChunks {
+						if !c.HasNext() {
+							t.Errorf("%s: c.HasNext() was false on blob %q buffer size %d, expecting next chunk %q", tc.name, tc.blob, bufSize, string(wantChunk.Data))
+						}
+						got, err := c.Next()
+						if err != nil {
+							t.Errorf("%s: c.Next() gave error %v on blob %q buffer size %d, expecting next chunk %q", tc.name, err, tc.blob, bufSize, string(wantChunk.Data))
+						}
+						gotChunks = append(gotChunks, got)
+						if i == reset {
+							c.Reset()
+							break
+						}
+					}
+					if diff := cmp.Diff(tc.wantChunks[:len(gotChunks)], gotChunks); diff != "" {
+						t.Errorf("%s: Chunker buffer size %d gave result diff (-want +got):\n%s", tc.name, bufSize, diff)
+					}
+					gotChunks = nil
+					if reset >= len(tc.wantChunks) {
+						continue
+					}
+					for _, wantChunk := range tc.wantChunks {
+						if !c.HasNext() {
+							t.Errorf("%s: c.HasNext() was false on blob %q buffer size %d, expecting next chunk %q", tc.name, tc.blob, bufSize, string(wantChunk.Data))
+						}
+						got, err := c.Next()
+						if err != nil {
+							t.Errorf("%s: c.Next() gave error %v on blob %q buffer size %d, expecting next chunk %q", tc.name, err, tc.blob, bufSize, string(wantChunk.Data))
+						}
+						gotChunks = append(gotChunks, got)
+					}
+					if diff := cmp.Diff(tc.wantChunks, gotChunks); diff != "" {
+						t.Errorf("%s: Chunker buffer size %d gave result diff (-want +got):\n%s", tc.name, bufSize, diff)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestChunkerErrors_ErrEOF(t *testing.T) {
+	c := NewFromBlob([]byte("12"), 2)
+	_, err := c.Next()
+	if err != nil {
+		t.Errorf("c.Next() gave error %v, expecting next chunk \"12\"", err)
+	}
+	got, err := c.Next()
+	if err == nil {
+		t.Errorf("c.Next() gave %v, %v, expecting _, error", got, err)
+	}
+}
+
+func TestChunkerErrors_ShortRead(t *testing.T) {
+	execRoot, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("failed to make temp dir: %v", err)
+	}
+	defer os.RemoveAll(execRoot)
+	blob := []byte("123")
+	path := filepath.Join(execRoot, "file")
+	if err := ioutil.WriteFile(path, blob, 0777); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	dg := digest.NewFromBlob([]byte("1234")) // We digest a blob that is longer than the actual one.
+	IOBufferSize = 10
+	// The error will be returned immediately, because the first buffer read will be shorter than expected.
+	c := NewFromFile(path, dg, 2)
+	got, err := c.Next()
+	if err == nil {
+		t.Errorf("c.Next() gave %v, %v, expecting _, error", got, err)
+	}
+
+	IOBufferSize = 3
+	// This time the error will be not be returned immediately, because the first buffer read will
+	// return the expected 3 bytes, and only the second one will be shorter than expected.
+	c = NewFromFile(path, dg, 2)
+	_, err = c.Next()
+	if err != nil {
+		t.Errorf("c.Next() gave error %v, expecting next chunk \"12\"", err)
+	}
+	got, err = c.Next()
+	if err == nil {
+		t.Errorf("c.Next() gave %v, %v, expecting _, error", got, err)
+	}
+}
+
+func TestChunkerResetOptimization_SmallFile(t *testing.T) {
+	// Files smaller than IOBufferSize are loaded into memory once and not re-read on Reset.
+	execRoot, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("failed to make temp dir: %v", err)
+	}
+	defer os.RemoveAll(execRoot)
+
+	blob := []byte("123")
+	path := filepath.Join(execRoot, "file")
+	if err := ioutil.WriteFile(path, blob, 0777); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	dg := digest.NewFromBlob(blob)
+	IOBufferSize = 10
+	c := NewFromFile(path, dg, 3)
+	got, err := c.Next()
+	if err != nil {
+		t.Errorf("c.Next() gave error %v", err)
+	}
+	wantChunk := &Chunk{Data: blob}
+	if diff := cmp.Diff(wantChunk, got); diff != "" {
+		t.Errorf("c.Next() gave result diff (-want +got):\n%s", diff)
+	}
+	c.Reset()
+	// Change the file contents.
+	if err := ioutil.WriteFile(path, []byte("321"), 0777); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	got, err = c.Next()
+	if err != nil {
+		t.Errorf("c.Next() gave error %v", err)
+	}
+	if diff := cmp.Diff(wantChunk, got); diff != "" {
+		t.Errorf("c.Next() gave result diff (-want +got):\n%s", diff)
+	}
+}
+
+func TestChunkerResetOptimization_FullData(t *testing.T) {
+	// After FullData is called once, the file contents will remain loaded into memory and not
+	// re-read on Reset, even if the file is larger than IOBufferSize.
+	execRoot, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Fatalf("failed to make temp dir: %v", err)
+	}
+	defer os.RemoveAll(execRoot)
+
+	blob := []byte("12345678")
+	path := filepath.Join(execRoot, "file")
+	if err := ioutil.WriteFile(path, blob, 0777); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	dg := digest.NewFromBlob(blob)
+	IOBufferSize = 5
+	c := NewFromFile(path, dg, 3)
+	got, err := c.FullData()
+	if err != nil {
+		t.Errorf("c.FullData() gave error %v", err)
+	}
+	if !bytes.Equal(got, blob) {
+		t.Errorf("c.FullData() gave result diff, want %q, got %q", string(blob), string(got))
+	}
+	c.Reset()
+	// Change the file contents.
+	if err := ioutil.WriteFile(path, []byte("987654321"), 0777); err != nil {
+		t.Fatalf("failed to write temp file: %v", err)
+	}
+	got, err = c.FullData()
+	if err != nil {
+		t.Errorf("c.FullData() gave error %v", err)
+	}
+	if !bytes.Equal(got, blob) {
+		t.Errorf("c.FullData() gave result diff, want %q, got %q", string(blob), string(got))
+	}
+}
