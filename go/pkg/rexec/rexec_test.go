ec538b14e7ea965c0d351f03f7bdec85cb87948e
Ola Rozenfeld <olaola@google.com>
2019-07-17 14:47:29 -0400

    Adding inputs metadata calculation (#45).

--- go/pkg/rexec/rexec_test.go
+++ go/pkg/rexec/rexec_test.go
@@ -52,7 +52,7 @@ func TestExecCacheHit(t *testing.T) {
 		CommandDigest:    cmdDg,
 		ActionDigest:     acDg,
 		InputDirectories: 1,
-		InputFiles:       1,
+		InputFiles:       2,
 		TotalInputBytes:  fooDirDg.Size + cmdDg.Size + acDg.Size + fooDg.Size,
 	}
 	if diff := cmp.Diff(wantRes, res); diff != "" {
