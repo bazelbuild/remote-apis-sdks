019d28e4138ca11999999a1c8407f612d81fd205
Ola Rozenfeld <olaola@google.com>
2020-09-17 12:40:27 -0400

    Adding download file and Merkle dir to remote tool. (#179)
    * Adding download file and Merkle dir to remote tool.
    
    * Addressing comments

--- go/cmd/remotetool/main.go
+++ go/cmd/remotetool/main.go
@@ -1,11 +1,18 @@
 // Main package for the remotetool binary.
 //
-// Here's a sample invocation of this tool to download an action result:
+// This tool supports common debugging operations concerning remotely executed
+// actions:
+// 1. Download a file or directory from remote cache by its digest.
+// 2. Display details of a remotely executed action.
+// 3. Download action results by the action digest.
+//
+// Example (download an action result from remote action cache):
 // bazelisk run //go/cmd/remotetool -- \
-// 	--instance=projects/rbe-android-ci/instances/default_instance \
+//  --operation=download_action_result \
+// 	--instance=$INSTANCE \
 // 	--service remotebuildexecution.googleapis.com:443 \
 // 	--alsologtostderr --v 1 \
-// 	--credential_file ~/.config/foundry/keys/dev-foundry.json \
+// 	--credential_file $CRED_FILE \
 // 	--digest=52a54724e6b3dff3bc44ef5dceb3aab5892f2fc7e37fce5aa6e16a7a266fbed6/147 \
 // 	--path=`pwd`/tmp
 package main
@@ -29,27 +36,22 @@ type OpType string
 const (
 	downloadActionResult OpType = "download_action_result"
 	showAction           OpType = "show_action"
+	downloadBlob         OpType = "download_blob"
+	downloadDir          OpType = "download_dir"
 )
+
+var supportedOps = []OpType{
+	downloadActionResult,
+	showAction,
+	downloadBlob,
+	downloadDir,
+}
 
 var (
-	operation  = flag.String("operation", "download_action_result", "Specifies the operation to perform. Currently only download_action_result is supported.")
+	operation  = flag.String("operation", "", fmt.Sprintf("Specifies the operation to perform. Supported values: %v", supportedOps))
 	digest     = flag.String("digest", "", "Digest in <digest/size_bytes> format.")
-	pathPrefix = flag.String("path", "/tmp", "Directory to which outputs should be downloaded to. Defaults to /tmp/.")
+	pathPrefix = flag.String("path", "", "Path to which outputs should be downloaded to.")
 )
-
-func validate() {
-	if *digest == "" {
-		log.Exitf("--digest must be specified.")
-	}
-
-	switch OpType(*operation) {
-	case downloadActionResult, showAction:
-		return
-
-	default:
-		log.Exitf("unsupported operation %q.", *operation)
-	}
-}
 
 func main() {
 	flag.Usage = func() {
@@ -57,7 +59,12 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-	validate()
+	if *operation == "" {
+		log.Exitf("--operation must be specified.")
+	}
+	if *digest == "" {
+		log.Exitf("--digest must be specified.")
+	}
 
 	ctx := context.Background()
 	grpcClient, err := rflags.NewClientFromFlags(ctx)
@@ -69,8 +76,26 @@ func main() {
 
 	switch OpType(*operation) {
 	case downloadActionResult:
+		if *pathPrefix == "" {
+			log.Exitf("--path must be specified.")
+		}
 		if err := c.DownloadActionResult(ctx, *digest, *pathPrefix); err != nil {
 			log.Exitf("error downloading action result for digest %v: %v", *digest, err)
+		}
+
+	case downloadBlob:
+		res, err := c.DownloadBlob(ctx, *digest, *pathPrefix)
+		if err != nil {
+			log.Exitf("error downloading blob for digest %v: %v", *digest, err)
+		}
+		fmt.Fprintf(os.Stdout, res)
+
+	case downloadDir:
+		if *pathPrefix == "" {
+			log.Exitf("--path must be specified.")
+		}
+		if err := c.DownloadDirectory(ctx, *digest, *pathPrefix); err != nil {
+			log.Exitf("error downloading directory for digest %v: %v", *digest, err)
 		}
 
 	case showAction:
@@ -81,6 +106,6 @@ func main() {
 		fmt.Fprintf(os.Stdout, res)
 
 	default:
-		log.Exitf("unsupported operation %v.", *operation)
+		log.Exitf("unsupported operation %v. Supported operations:\n%v", *operation, supportedOps)
 	}
 }
--- go/pkg/tool/tool_test.go
+++ go/pkg/tool/tool_test.go
@@ -73,22 +73,63 @@ func TestTool_ShowAction(t *testing.T) {
 	}
 	want := `Command
 ========
-Command Digest: 76a608e419da9ed3673f59b8b903f21dbf7cc3178281029151a090cac02d9e4d
+Command Digest: 76a608e419da9ed3673f59b8b903f21dbf7cc3178281029151a090cac02d9e4d/15
 	tool
 
 Inputs
 ======
-a: [Directory digest: b2fcb5f1407e2324dd81c4685724cac9228c7142cb34d665dfd3f37a8a18342c]
+a: [Directory digest: b2fcb5f1407e2324dd81c4685724cac9228c7142cb34d665dfd3f37a8a18342c/75]
 a/b/input.txt: [File digest: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/0]
-
 
 Output Files:
 =============
-a/b/out, digest: e0ee8bb50685e05fa0f47ed04203ae953fdfd055f5bd2892ea186504254f8c3a
+a/b/out, digest: e0ee8bb50685e05fa0f47ed04203ae953fdfd055f5bd2892ea186504254f8c3a/6
 
 Output Files From Directories:
-=================`
+=================
+`
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Fatalf("ShowAction(%v) returned diff (-want +got): %v\n\ngot: %v\n\nwant: %v\n", acDg.String(), diff, got, want)
+	}
+}
+
+func TestTool_DownloadBlob(t *testing.T) {
+	e, cleanup := fakes.NewTestEnv(t)
+	defer cleanup()
+	cas := e.Server.CAS
+	dg := cas.Put([]byte("hello"))
+
+	toolClient := &Client{GrpcClient: e.Client.GrpcClient}
+	got, err := toolClient.DownloadBlob(context.Background(), dg.String(), "")
+	if err != nil {
+		t.Fatalf("DownloadBlob(%v) failed: %v", dg.String(), err)
+	}
+	want := "hello"
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("DownloadBlob(%v) returned diff (-want +got): %v\n\ngot: %v\n\nwant: %v\n", dg.String(), diff, got, want)
+	}
+	// Now download into a specified location.
+	tmpFile, err := ioutil.TempFile(os.TempDir(), "")
+	if err != nil {
+		t.Fatalf("TempFile failed: %v", err)
+	}
+	if err := tmpFile.Close(); err != nil {
+		t.Fatalf("TempFile Close failed: %v", err)
+	}
+	fp := tmpFile.Name()
+	got, err = toolClient.DownloadBlob(context.Background(), dg.String(), fp)
+	if err != nil {
+		t.Fatalf("DownloadBlob(%v) failed: %v", dg.String(), err)
+	}
+	if got != "" {
+		t.Fatalf("DownloadBlob(%v) returned %v, expected empty: ", dg.String(), got)
+	}
+	c, err := ioutil.ReadFile(fp)
+	if err != nil {
+		t.Fatalf("Unable to read downloaded output file %v: %v", fp, err)
+	}
+	got = string(c)
+	if got != want {
+		t.Fatalf("Incorrect content in downloaded file %v, want %v, got %v", fp, want, got)
 	}
 }
