172f2fcfc410048b710fe3769d3623d706b29bcd
Rubens Farias <rubensf@google.com>
2020-11-11 14:09:08 -0500

    Make Chunker.Next read "generic" and independent from digest size. (#236)
    * Make Chunker.Next read "generic" and independent from digest size.
    
    These are both pre-works for adding write compression support to the
    remote-apis-sdks.
    
    Chunker.Next should be "generic" as it allows for a drop in implementation
    of a reader that compresses on the fly. I still left the special case of
    caching data in memory as it saves the effort from memory copies.
    
    Making the chunker reads work independently from the digest size is also
    useful as it allows us to not have to pre-compute the digest of the
    compressed blobs. The current draft of the RE API never requires the
    digest of the compressed blob at any point, and this saves us the
    trouble of needing to read the data twice.
    
    Notice that this implies that the chunker won't be actually matching
    data against the supplied digest at any moment. The digest is now purely
    information storage, rather than necessary for chunker logic.
    
    As a caveat, for simplicity, I made it that we only cache in memory
    files that are smaller than the *chunk* size rather than the IO buffer
    size.
    
    * Break reader in its own package.
    
    * Change Seek -> SeekOffset so govet will stop complaining.
    
    * Fix nit typo
    
    * Use test_util.CreateFile
    
    * Converge io.EOF and io.UnexpectedEOF errors cases
    
    * Re-add t.Parallel

---
+++ go/pkg/reader/reader_test.go
@@ -0,0 +1,113 @@
+package reader
+
+import (
+	"io"
+	"testing"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/test_util"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFileReaderSeeks(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name         string
+		IOBuffSize   int
+		dataBuffSize int
+		blob         string
+		seekOffset   int64
+		wantResetErr error
+	}{
+		{
+			name:         "Smaller data buffer",
+			IOBuffSize:   10,
+			dataBuffSize: 3,
+			blob:         "1234567",
+			seekOffset:   2,
+			wantResetErr: nil,
+		},
+		{
+			name:         "Smaller io buffer",
+			IOBuffSize:   1,
+			dataBuffSize: 3,
+			blob:         "1234567",
+			seekOffset:   2,
+			wantResetErr: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			path, err := test_util.CreateFile(t, false, tc.blob)
+			if err != nil {
+				t.Fatalf("Failed to make temp file: %v", err)
+			}
+
+			data := make([]byte, tc.dataBuffSize)
+
+			r := NewFileReadSeeker(path, tc.IOBuffSize)
+			if _, err := r.Read(data); err == nil {
+				t.Errorf("Read() = should have err'd on unitialized reader")
+			}
+			if err := r.Initialize(); err != nil {
+				t.Fatalf("Failed to initialize reader: %v", err)
+			}
+
+			n, err := io.ReadFull(r, data)
+			if n != tc.dataBuffSize {
+				t.Errorf("Read() = %d bytes, expected %d", n, tc.dataBuffSize)
+			}
+			if err != nil {
+				t.Errorf("Read() = %v err, expected nil", err)
+			}
+			if diff := cmp.Diff(string(data), tc.blob[:tc.dataBuffSize]); diff != "" {
+				t.Errorf("Read() = incorrect result, diff(-want, +got): %v", diff)
+			}
+
+			r.SeekOffset(tc.seekOffset)
+			if _, err := r.Read(data); err == nil {
+				t.Errorf("Read() = should have err'd on unitialized reader")
+			}
+			if err := r.Initialize(); err != nil {
+				t.Fatalf("Failed to initialize reader: %v", err)
+			}
+
+			n, err = io.ReadFull(r, data)
+			if n != tc.dataBuffSize {
+				t.Errorf("Read() = %d bytes, expected %d", n, tc.dataBuffSize)
+			}
+			if err != nil {
+				t.Errorf("Read() = %v err, expected nil", err)
+			}
+			endRead := int(tc.seekOffset) + tc.dataBuffSize
+			if endRead > len(tc.blob) {
+				endRead = len(tc.blob)
+			}
+			if diff := cmp.Diff(string(data), tc.blob[tc.seekOffset:endRead]); diff != "" {
+				t.Errorf("Read() = incorrect result, diff(-want, +got): %v", diff)
+			}
+		})
+	}
+
+}
+
+func TestFileReaderSeeksPastOffset(t *testing.T) {
+	t.Parallel()
+	path, err := test_util.CreateFile(t, false, "12345")
+	if err != nil {
+		t.Fatalf("Failed to make temp file: %v", err)
+	}
+
+	r := NewFileReadSeeker(path, 10)
+	// Past Offset
+	r.SeekOffset(10)
+	if err := r.Initialize(); err != nil {
+		t.Fatalf("Failed to initialize reader: %v", err)
+	}
+
+	data := make([]byte, 1)
+	if _, err := r.Read(data); err == nil {
+		t.Errorf("Expected err, got nil")
+	}
+}
