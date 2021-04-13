a75f19c3b997c1ecfaaa73611f8a6874c1713b39
Rubens Farias <rubensf@google.com>
2021-04-13 21:20:43 +0000

    perf tool

--- go/cmd/remotetool/main.go
+++ go/cmd/remotetool/main.go
@@ -19,12 +19,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"os"
 	"path"
+	"runtime/pprof"
+	"strings"
+	"time"
 
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/client"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/command"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/filemetadata"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/outerr"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/tool"
 
@@ -43,6 +50,7 @@ const (
 	reexecuteAction      OpType = "reexecute_action"
 	checkDeterminism     OpType = "check_determinism"
 	uploadBlob           OpType = "upload_blob"
+	computeTree          OpType = "compute_tree"
 )
 
 var supportedOps = []OpType{
@@ -61,6 +69,7 @@ var (
 	pathPrefix   = flag.String("path", "", "Path to which outputs should be downloaded to.")
 	inputRoot    = flag.String("input_root", "", "For reexecute_action: if specified, override the action inputs with the specified input root.")
 	execAttempts = flag.Int("exec_attempts", 10, "For check_determinism: the number of times to remotely execute the action and check for mismatches.")
+	cpuProfFile  = flag.String("pprof_file", "", "File to dump pprof.")
 )
 
 func main() {
@@ -74,6 +83,18 @@ func main() {
 	}
 	if *execAttempts <= 0 {
 		log.Exitf("--exec_attempts must be >= 1.")
+	}
+
+	if *cpuProfFile != "" {
+		f, err := os.Create(*cpuProfFile)
+		if err != nil {
+			log.Fatal("Could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("Could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
 	}
 
 	ctx := context.Background()
@@ -124,6 +145,9 @@ func main() {
 			log.Exitf("error uploading blob for digest %v: %v", getDigestFlag(), err)
 		}
 
+	case computeTree:
+		ComputeTree(grpcClient)
+
 	default:
 		log.Exitf("unsupported operation %v. Supported operations:\n%v", *operation, supportedOps)
 	}
@@ -141,4 +165,51 @@ func getPathFlag() string {
 		log.Exitf("--path must be specified.")
 	}
 	return *pathPrefix
+}
+
+func ComputeTree(grpcClient *client.Client) {
+	totalRuns := 0
+	beg := time.Now()
+	defer func() {
+		log.Infof("Ran %v commands in %v time", totalRuns, time.Since(beg).Milliseconds())
+	}()
+
+	file, err := os.Open(getPathFlag())
+	if err != nil {
+		log.Exitf("failed to open input")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	fmc := filemetadata.NewSingleFlightCache()
+	is := command.InputSpec{}
+	for scanner.Scan() {
+		txt := scanner.Text()
+
+		if strings.TrimSpace(txt) == "" {
+			if len(is.Inputs) != 0 || len(is.VirtualInputs) != 0 {
+				grpcClient.ComputeMerkleTree(*inputRoot, &is, fmc)
+				totalRuns += 1
+				is = command.InputSpec{}
+			}
+
+			continue
+		}
+
+		brokenTxt := strings.Split(txt, ":")
+		if len(brokenTxt) != 2 {
+			log.Exitf("broken line %v", txt)
+		}
+		key := strings.TrimSpace(brokenTxt[0])
+		v := strings.TrimSpace(brokenTxt[1])
+
+		if key == "inputs" {
+			is.Inputs = append(is.Inputs, v)
+		} else if key == "path" {
+			is.VirtualInputs = append(is.VirtualInputs, &command.VirtualInput{Path: v})
+		} else {
+			log.Exitf("broken line %v", txt)
+		}
+	}
 }
