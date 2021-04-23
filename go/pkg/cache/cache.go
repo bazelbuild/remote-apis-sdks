69c6642c3a006636256c1d2e591899f07d4c74bf
nodirg <56001730+nodirg@users.noreply.github.com>
2021-04-23 09:20:14 -0700

    Simplify caching packages (#303)
    * Simplify caching packages
    
    Simplify and optimize singleflightcache implementation: replace two
    sync.Maps and an N+1 sync.RWMutexes, with one sync.Map and N sync.Once.
    Do other simplifications as well.
    
    Remove dead code / impossible conditions, e.g. "unexpected type found in
    lock map". Use that to simplify the function signatures, e.g. don't
    return an error (and thus simplify callsites and tests) when an error
    is impossible.
    
    In filemetadata/cache.go, fix the bug that it resets all namespaces.
    
    Reuse singleflightcache in cas package
    
    `cache.Cache.Reset` was inherently racy because it was overwriting a mutex (without holding another mutex).
    Replace with safe ns-level resetting.

--- go/pkg/cache/cache.go
+++ go/pkg/cache/cache.go
@@ -2,7 +2,6 @@
 package cache
 
 import (
-	"fmt"
 	"sync"
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/cache/singleflightcache"
@@ -10,31 +9,29 @@ import (
 
 // Cache is a cache backend.
 type Cache struct {
-	mu     *sync.RWMutex
-	caches *sync.Map
+	caches sync.Map
 }
 
-var (
-	instance *Cache
-	once     sync.Once
-)
+var instance Cache
 
 // GetInstance retrieves the singleton instance of the cache backend. This is useful in the
 // future to enforce memory bounds on the entire cache usage of a program.
 func GetInstance() *Cache {
-	once.Do(func() {
-		instance = &Cache{
-			mu:     &sync.RWMutex{},
-			caches: &sync.Map{},
-		}
-	})
-	return instance
+	return &instance
 }
 
-// Reset resets the cache.
-func (c *Cache) Reset() {
-	c.mu = &sync.RWMutex{}
-	c.caches = &sync.Map{}
+// Reset resets the cache in one namespace.
+func (c *Cache) Reset(ns string) {
+	c.caches.Delete(ns)
+}
+
+func (c *Cache) getNSCache(ns string) *singleflightcache.Cache {
+	// Load first to avoid instantiating a new cache for LoadOrStore.
+	nsCache, ok := c.caches.Load(ns)
+	if !ok {
+		nsCache, _ = c.caches.LoadOrStore(ns, &singleflightcache.Cache{})
+	}
+	return nsCache.(*singleflightcache.Cache)
 }
 
 // LoadOrStore attempts to first read a value from the corresponding cache namespace. If no entry
@@ -42,45 +39,17 @@ func (c *Cache) Reset() {
 // callers of LoadOrStore on the same namespace and key will execute fn once. This is to avoid
 // costly redundant work to compute the value to store in cache.
 func (c *Cache) LoadOrStore(ns string, key interface{}, fn func() (interface{}, error)) (interface{}, error) {
-	// Load first to avoid instantiating a new cache for LoadOrStore.
-	nsCache, ok := c.caches.Load(ns)
-	if !ok {
-		nsCache, _ = c.caches.LoadOrStore(ns, &singleflightcache.Cache{})
-	}
-	cache, ok := nsCache.(*singleflightcache.Cache)
-	if !ok {
-		return nil, fmt.Errorf("unexpected type in namespace cache map")
-	}
-	return cache.LoadOrStore(key, fn)
+	return c.getNSCache(ns).LoadOrStore(key, fn)
 }
 
 // Store is similar to LoadOrStore, except it does not check if a cache entry
 // already exists for the given key and simply overwrites the value of the key
 // in the cache with the given value.
-func (c *Cache) Store(ns string, key interface{}, val interface{}) error {
-	// Load first to avoid instantiating a new cache for LoadOrStore.
-	nsCache, ok := c.caches.Load(ns)
-	if !ok {
-		nsCache, _ = c.caches.LoadOrStore(ns, &singleflightcache.Cache{})
-	}
-	cache, ok := nsCache.(*singleflightcache.Cache)
-	if !ok {
-		return fmt.Errorf("unexpected type in namespace cache map")
-	}
-	return cache.Store(key, val)
+func (c *Cache) Store(ns string, key interface{}, val interface{}) {
+	c.getNSCache(ns).Store(key, val)
 }
 
 // Delete deletes a value corresponding to the given namespace and key.
-func (c *Cache) Delete(ns string, key interface{}) error {
-	// Load first to avoid instantiating a new cache for LoadOrStore.
-	nsCache, ok := c.caches.Load(ns)
-	if !ok {
-		nsCache, _ = c.caches.LoadOrStore(ns, &singleflightcache.Cache{})
-	}
-	cache, ok := nsCache.(*singleflightcache.Cache)
-	if !ok {
-		return fmt.Errorf("unexpected type in namespace cache map")
-	}
-	cache.Delete(key)
-	return nil
+func (c *Cache) Delete(ns string, key interface{}) {
+	c.getNSCache(ns).Delete(key)
 }
