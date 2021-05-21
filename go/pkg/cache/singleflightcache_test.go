413b86b524d6ec25719d4603f00274914e70a0ca
Rubens Farias <rubensf@google.com>
2021-05-21 10:49:46 -0400

    Address comments

--- go/pkg/cache/singleflightcache_test.go
+++ go/pkg/cache/singleflightcache_test.go
@@ -69,7 +69,7 @@ func TestValFnFailure(t *testing.T) {
 	fnErr := errors.New("error")
 	val, err := s.LoadOrStore(key1, func() (interface{}, error) { return nil, fnErr })
 	if err == nil {
-		t.Errorf("LoadOrStore(%v) failed: val - %v, err is nil", key1, val)
+		t.Errorf("LoadOrStore(%v) failed: val is %v, err is nil", key1, val)
 	}
 
 	val, err = s.LoadOrStore(key1, func() (interface{}, error) { return val1, nil })
