6826b88b2bac1efc88ea9626e346120f3d13a36a
Peter Ebden <peter.ebden@gmail.com>
2023-04-26 22:19:04 +0100

    Handle symlinks in packageDirectories (#441)

--- go/pkg/client/tree.go
+++ go/pkg/client/tree.go
@@ -529,6 +529,12 @@ func packageDirectories(t *treeNode) (root *repb.Directory, files map[digest.Dig
 		files[dg] = fn.ue
 	}
 	sort.Slice(root.Files, func(i, j int) bool { return root.Files[i].Name < root.Files[j].Name })
+
+	for name, sym := range t.symlinks {
+		root.Symlinks = append(root.Symlinks, &repb.SymlinkNode{Name: name, Target: sym.target})
+	}
+	sort.Slice(root.Symlinks, func(i, j int) bool { return root.Symlinks[i].Name < root.Symlinks[j].Name })
+
 	return root, files, treePb, nil
 }
 
