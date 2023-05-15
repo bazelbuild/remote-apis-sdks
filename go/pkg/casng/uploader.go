9e745c5a4faa3aae34a66badad0ff0dc1804d5e6
Anas Sulaiman <anas@sulaiman.pro>
2023-05-15 20:33:08 +0000

    introduce casng package
    This PR includes one method and some of the scaffolding for other
    methods that will be posted later.

---
+++ go/pkg/casng/uploader.go
@@ -0,0 +1,270 @@
+// This file includes the implementation for uploading blobs to the CAS.
+//
+// The following diagram illustrates the overview of the design implemented in this package.
+// The request follows a linear path through the system: request -> digest -> query -> upload -> response.
+// Each box represents a processor with its own state to manage concurrent requests and proper messaging with other processors.
+/*
+
+
+                               Dispatcher
+                    ┌─────────────────────────┐
+                    │                         │
+     ┌───────────┐  │ ┌─────┐       ┌──────┐  │ Digest
+     │           │  │ │     │ Digest│ Pipe ├──┼───────┐
+     │ Digester  ├──┼─► Req ├───────► Req  │  │       │
+     │           │  │ └─────┘       └──────┘  │  ┌────▼─────┐
+     └─────▲─────┘  │                         │  │          │
+   Upload  │        │                         │  │  Query   │
+   Request │        │                         │  │ Processor│
+           │        │                         │  │          │
+      ┌────┴───┐    │ ┌─────┐ Cache ┌──────┐  │  └────┬─────┘
+      │        ◄────┼─┤ Res │  Hit  │ Pipe │  │       │
+      │  User  │    │ │     ◄───────┤ Res  ◄──┼───────┘
+      │        │    │ └▲──▲─┘       └┬────┬┘  │  Query
+      └────────┘    │  │  │     Small│    │   │ Response
+                    │  │  │     Blob │    │   │
+                    └──┼──┼──────────┼────┼───┘
+                       │  │          │    │
+                       │  │ ┌────────▼─┐  │Large
+                       │  │ │  Batcher │  │Blob
+                       │  └─┤   gRPC   │  │
+                       │    └──────────┘  │
+                       │                  │
+                       │    ┌──────────┐  │
+                       │    │ Streamer │  │
+                       └────┤   gRPC   ◄──┘
+                            └──────────┘
+*/
+// The overall streaming flow is as follows:
+//   digester        -> dispatcher/req
+//   dispatcher/req  -> dispatcher/pipe
+//   dispatcher/pipe -> query processor
+//   query processor -> dispatcher/pipe
+//   dispatcher/pipe -> dispatcher/res (cache hit)
+//   dispatcher/res  -> requester (cache hit)
+//   dispatcher/pipe -> batcher (small file)
+//   dispatcher/pipe -> streamer (medium and large file)
+//   batcher         -> dispatcher/res
+//   streamer        -> dispatcher/res
+//   dispatcher/res  -> requester
+//
+// The termination sequence is as follows:
+//   user cancels the batching or the streaming context, not the uploader's context, and closes input streaming channels.
+//       cancelling the context triggers aborting in-flight requests.
+//   user cancels uploader's context: cancels pending digestions and gRPC processors blocked on throttlers.
+//   client senders (top level) terminate.
+//   the digester channel is closed, and a termination signal is sent to the dispatcher.
+//   the dispatcher terminates its sender and propagates the signal to its piper.
+//   the dispatcher's piper propagtes the signal to the intermediate query streamer.
+//   the intermediate query streamer terimnates and propagates the signal to the query processor and dispatcher's piper.
+//   the query processor terminates.
+//   the dispatcher's piper terminates.
+//   the dispatcher's counter termiantes (after observing all the remaining blobs) and propagates the signal to the receiver.
+//   the dispatcher's receiver terminates.
+//   the dispatcher terminates and propagates the signal to the batcher and the streamer.
+//   the batcher and the streamer terminate.
+//   user waits for the termination signal: return from batching uploader or response channel closed from streaming uploader.
+//       this ensures the whole pipeline is drained properly.
+
+// A note about logging:
+//
+//		Level 1 is used for top-level functions, typically called once during the lifetime of the process or initiated by the user.
+//		Level 2 is used for internal functions that may be called per request.
+//		Level 3 is used for internal functions that may be called multiple times per request. Duration logs are also level 3 to avoid the overhead in level 4.
+//	 Level 4 is used for messages with large objects.
+//	 Level 5 is used for messages that require custom processing (extra compute).
+package casng
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/retry"
+	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	log "github.com/golang/glog"
+	"github.com/klauspost/compress/zstd"
+	"github.com/pborman/uuid"
+	bspb "google.golang.org/genproto/googleapis/bytestream"
+	"google.golang.org/protobuf/proto"
+)
+
+var (
+	// ErrNilClient indicates an invalid nil argument.
+	ErrNilClient = errors.New("client cannot be nil")
+
+	// ErrCompression indicates an error in the compression routine.
+	ErrCompression = errors.New("compression error")
+
+	// ErrIO indicates an error in an IO routine.
+	ErrIO = errors.New("io error")
+
+	// ErrGRPC indicates an error in a gRPC routine.
+	ErrGRPC = errors.New("grpc error")
+
+	// ErrOversizedItem indicates an item that is too large to fit into the set byte limit for the corresponding gRPC call.
+	ErrOversizedItem = errors.New("oversized item")
+
+	// ErrTerminatedUploader indicates an attempt to use a terminated uploader.
+	ErrTerminatedUploader = errors.New("cannot use a terminated uploader")
+)
+
+// MakeWriteResourceName returns a valid resource name for writing an uncompressed blob.
+func MakeWriteResourceName(instanceName, hash string, size int64) string {
+	return fmt.Sprintf("%s/uploads/%s/blobs/%s/%d", instanceName, uuid.New(), hash, size)
+}
+
+// MakeCompressedWriteResourceName returns a valid resource name for writing a compressed blob.
+func MakeCompressedWriteResourceName(instanceName, hash string, size int64) string {
+	return fmt.Sprintf("%s/uploads/%s/compressed-blobs/zstd/%s/%d", instanceName, uuid.New(), hash, size)
+}
+
+// BatchingUplodaer provides a blocking interface to query and upload to the CAS.
+type BatchingUploader struct {
+	*uploader
+}
+
+// StreamingUploader provides an non-blocking interface to query and upload to the CAS
+type StreamingUploader struct {
+	*uploader
+}
+
+// uploader represents the state of an uploader implementation.
+type uploader struct {
+	cas          repb.ContentAddressableStorageClient
+	byteStream   bspb.ByteStreamClient
+	instanceName string
+
+	queryRPCCfg  GRPCConfig
+	batchRPCCfg  GRPCConfig
+	streamRPCCfg GRPCConfig
+
+	// IO controls.
+	ioCfg        IOConfig
+	buffers      sync.Pool
+	zstdEncoders sync.Pool
+	// nodeCache allows digesting each path only once.
+	// Concurrent walkers claim a path by storing a sync.WaitGroup reference, which allows other walkers to defer
+	// digesting that path until the first walker stores the digest once it's computed.
+	// The keys are unique per walk, which means two walkers with different filters may cache the same path twice, but each copy could
+	// have a different node associated with it.
+	// However, regular files will have duplicate nodes in this cache.
+	nodeCache sync.Map
+	// fileNodeCache is similar to nodeCache, but only holds file nodes. The keys are real paths and are not unique across walks.
+	// This cache ensures that regular files are only digested once, even across walks with different exclusion filters.
+	// It also ensures that nodeCache does not have duplicate nodes for identical files.
+	// In other words, nodeCache might hold different views of the same directory node, but fileNodeCache will always hold the canonical file node for the corresponding real path.
+	// Since nodes are pointer-like references, the shared memory cost between the two caches is limited to keys and addresses.
+	fileNodeCache             sync.Map
+	queryRequestBaseSize      int
+	uploadRequestBaseSize     int
+	uploadRequestItemBaseSize int
+
+	// Concurrency controls.
+	clientSenderWg sync.WaitGroup // Batching API producers.
+	querySenderWg  sync.WaitGroup // Query streaming API producers.
+	uploadSenderWg sync.WaitGroup // Upload streaming API producers.
+	processorWg    sync.WaitGroup // Internal routers.
+	receiverWg     sync.WaitGroup // Consumers.
+	workerWg       sync.WaitGroup // Short-lived intermediate producers/consumers.
+	walkerWg       sync.WaitGroup // Tracks all walkers.
+
+	// ctx is used to make unified calls and terminate saturated throttlers and in-flight workers.
+	ctx context.Context
+}
+
+// NewBatchingUploader creates a new instance of the batching uploader.
+// WIP: While this is intended to replace the uploader in the client and cas packages, it is not yet ready for production envionrments.
+//
+// The specified configs must be compatible with the capabilities of the server that the specified clients are connected to.
+// ctx must be cancelled after all batching calls have returned to properly shutdown the uploader. It is only used for cancellation (not used with remote calls).
+// gRPC timeouts are multiplied by retries. Batched RPCs are retried per batch. Streaming PRCs are retried per chunk.
+func NewBatchingUploader(
+	ctx context.Context, cas repb.ContentAddressableStorageClient, byteStream bspb.ByteStreamClient, instanceName string,
+	queryCfg, batchCfg, streamCfg GRPCConfig, ioCfg IOConfig,
+) (*BatchingUploader, error) {
+	uploader, err := newUploader(ctx, cas, byteStream, instanceName, queryCfg, batchCfg, streamCfg, ioCfg)
+	if err != nil {
+		return nil, err
+	}
+	return &BatchingUploader{uploader: uploader}, nil
+}
+
+// NewStreamingUploader creates a new instance of the streaming uploader.
+// WIP: While this is intended to replace the uploader in the client and cas packages, it is not yet ready for production envionrments.
+//
+// The specified configs must be compatible with the capabilities of the server which the specified clients are connected to.
+// ctx must be cancelled after all response channels have been closed to properly shutdown the uploader. It is only used for cancellation (not used with remote calls).
+// gRPC timeouts are multiplied by retries. Batched RPCs are retried per batch. Streaming PRCs are retried per chunk.
+func NewStreamingUploader(
+	ctx context.Context, cas repb.ContentAddressableStorageClient, byteStream bspb.ByteStreamClient, instanceName string,
+	queryCfg, batchCfg, streamCfg GRPCConfig, ioCfg IOConfig,
+) (*StreamingUploader, error) {
+	uploader, err := newUploader(ctx, cas, byteStream, instanceName, queryCfg, batchCfg, streamCfg, ioCfg)
+	if err != nil {
+		return nil, err
+	}
+	return &StreamingUploader{uploader: uploader}, nil
+}
+
+func newUploader(
+	ctx context.Context, cas repb.ContentAddressableStorageClient, byteStream bspb.ByteStreamClient, instanceName string,
+	queryCfg, uploadCfg, streamCfg GRPCConfig, ioCfg IOConfig,
+) (*uploader, error) {
+	if cas == nil || byteStream == nil {
+		return nil, ErrNilClient
+	}
+	if err := validateGrpcConfig(&queryCfg); err != nil {
+		return nil, err
+	}
+	if err := validateGrpcConfig(&uploadCfg); err != nil {
+		return nil, err
+	}
+	if err := validateGrpcConfig(&streamCfg); err != nil {
+		return nil, err
+	}
+	if err := validateIOConfig(&ioCfg); err != nil {
+		return nil, err
+	}
+
+	u := &uploader{
+		ctx: ctx,
+
+		cas:          cas,
+		byteStream:   byteStream,
+		instanceName: instanceName,
+
+		queryRPCCfg:  queryCfg,
+		batchRPCCfg:  uploadCfg,
+		streamRPCCfg: streamCfg,
+
+		ioCfg: ioCfg,
+		buffers: sync.Pool{
+			New: func() any {
+				// Since the buffers are never resized, treating the slice as a pointer-like type for this pool is safe.
+				buf := make([]byte, ioCfg.BufferSize)
+				return buf
+			},
+		},
+		zstdEncoders: sync.Pool{
+			New: func() any {
+				// Providing a nil writer implies that the encoder needs to be (re)initilaized with a writer using enc.Reset(w) before using it.
+				enc, _ := zstd.NewWriter(nil)
+				return enc
+			},
+		},
+
+		queryRequestBaseSize:      proto.Size(&repb.FindMissingBlobsRequest{InstanceName: instanceName, BlobDigests: []*repb.Digest{}}),
+		uploadRequestBaseSize:     proto.Size(&repb.BatchUpdateBlobsRequest{InstanceName: instanceName, Requests: []*repb.BatchUpdateBlobsRequest_Request{}}),
+		uploadRequestItemBaseSize: proto.Size(&repb.BatchUpdateBlobsRequest_Request{Digest: digest.NewFromBlob([]byte("abc")).ToProto(), Data: []byte{}}),
+	}
+	log.V(1).Infof("[casng] uploader.new: cfg_query=%+v, cfg_batch=%+v, cfg_stream=%+v, cfg_io=%+v", queryCfg, uploadCfg, streamCfg, ioCfg)
+
+	return u, nil
+}
+
+func (u *uploader) withRetry(ctx context.Context, predicate retry.ShouldRetry, policy retry.BackoffPolicy, fn func() error) error {
+	return retry.WithPolicy(ctx, predicate, policy, fn)
+}
---
+++ go/pkg/casng/uploader_test.go
@@ -0,0 +1,98 @@
+// Using a different package name to strictly exclude types defined here from the original package.
+package casng_test
+
+import (
+	"context"
+	"time"
+
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/casng"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/retry"
+	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	bspb "google.golang.org/genproto/googleapis/bytestream"
+	"google.golang.org/grpc"
+)
+
+var (
+	retryNever     = retry.Immediately(retry.Attempts(1))
+	retryTwice     = retry.ExponentialBackoff(time.Microsecond, time.Microsecond, retry.Attempts(2))
+	retryAllErrors = func(error) bool { return true }
+	defaultRPCCfg  = casng.GRPCConfig{
+		ConcurrentCallsLimit: 5,
+		ItemsLimit:           2,
+		BytesLimit:           1024,
+		Timeout:              time.Second,
+		BundleTimeout:        time.Millisecond,
+		RetryPolicy:          retryNever,
+		RetryPredicate:       retry.TransientOnly,
+	}
+	defaultIOCfg = casng.IOConfig{
+		ConcurrentWalksLimit:     1,
+		OpenFilesLimit:           1,
+		OpenLargeFilesLimit:      1,
+		SmallFileSizeThreshold:   1,
+		LargeFileSizeThreshold:   1,
+		CompressionSizeThreshold: 10,
+		BufferSize:               2,
+	}
+)
+
+type fakeByteStreamClient struct {
+	bspb.ByteStreamClient
+	write func(ctx context.Context, opts ...grpc.CallOption) (bspb.ByteStream_WriteClient, error)
+}
+
+type fakeByteStream_WriteClient struct {
+	bspb.ByteStream_WriteClient
+	send         func(*bspb.WriteRequest) error
+	closeAndRecv func() (*bspb.WriteResponse, error)
+}
+
+func (s *fakeByteStreamClient) Write(ctx context.Context, opts ...grpc.CallOption) (bspb.ByteStream_WriteClient, error) {
+	if s.write != nil {
+		return s.write(ctx, opts...)
+	}
+	return &fakeByteStream_WriteClient{}, nil
+}
+
+func (s *fakeByteStream_WriteClient) Send(wr *bspb.WriteRequest) error {
+	if s.send != nil {
+		return s.send(wr)
+	}
+	return nil
+}
+
+func (s *fakeByteStream_WriteClient) CloseAndRecv() (*bspb.WriteResponse, error) {
+	if s.closeAndRecv != nil {
+		return s.closeAndRecv()
+	}
+	return &bspb.WriteResponse{}, nil
+}
+
+type fakeCAS struct {
+	repb.ContentAddressableStorageClient
+	findMissingBlobs func(ctx context.Context, in *repb.FindMissingBlobsRequest, opts ...grpc.CallOption) (*repb.FindMissingBlobsResponse, error)
+	batchUpdateBlobs func(ctx context.Context, in *repb.BatchUpdateBlobsRequest, opts ...grpc.CallOption) (*repb.BatchUpdateBlobsResponse, error)
+}
+
+func (c *fakeCAS) FindMissingBlobs(ctx context.Context, in *repb.FindMissingBlobsRequest, opts ...grpc.CallOption) (*repb.FindMissingBlobsResponse, error) {
+	return c.findMissingBlobs(ctx, in, opts...)
+}
+
+func (c *fakeCAS) BatchUpdateBlobs(ctx context.Context, in *repb.BatchUpdateBlobsRequest, opts ...grpc.CallOption) (*repb.BatchUpdateBlobsResponse, error) {
+	return c.batchUpdateBlobs(ctx, in, opts...)
+}
+
+type byHash []digest.Digest
+
+func (a byHash) Len() int {
+	return len(a)
+}
+
+func (a byHash) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a byHash) Less(i, j int) bool {
+	return a[i].Hash < a[j].Hash
+}
