bazelbuild/remote-apis-sdks

commit e0c399322a6478ac3387452d68c1bd010fdf5031

Tool to show inputs/outputs & command of an action (#161)
The tool traverses both input and output directories recursively and
shows:
1. Empty directories
2. Files
3. Symlinks

The previous tool based on tools_remote only used to show the top level
output directories and didn't support symlinks.

As part of this change, I also:
1. Added fake GetTree() function (this was also a feature reequest from
the Goma team).
2. Added support for fake inputs in SDK.
3. Rearranged some of the fake server setup code so that options are
applied first before Merkle tree is computed. I'm doing this since fake
inputs need to be created from the options before Merkle tree is
computed.

Test: Unit-tests, variety of inteegration tests against actual action
digests in test stack.

go/pkg/fakes/server.go
@@ -2,6 +2,7 @@ package fakes
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"os"
@@ -15,6 +16,7 @@ import (
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/filemetadata"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/rexec"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/tree"
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -135,22 +137,7 @@ func timeToProto(t time.Time) *tspb.Timestamp {
 func (e *TestEnv) Set(cmd *command.Command, opt *command.ExecutionOptions, res *command.Result, opts ...option) (cmdDg, acDg digest.Digest) {
 	e.t.Helper()
 	cmd.FillDefaultFieldValues()
-	root, _, _, err := tree.ComputeMerkleTree(cmd.ExecRoot, cmd.InputSpec, int(e.Client.GrpcClient.ChunkMaxSize), e.Client.FileMetadataCache)
-	if err != nil {
-		e.t.Fatalf("error building input tree in fake setup: %v", err)
-		return digest.Empty, digest.Empty
-	}
 
-	cmdPb := cmd.ToREProto()
-	cmdDg = digest.TestNewFromMessage(cmdPb)
-	ac := &repb.Action{
-		CommandDigest:   cmdDg.ToProto(),
-		InputRootDigest: root.ToProto(),
-		DoNotCache:      opt.DoNotCache,
-	}
-	if cmd.Timeout > 0 {
-		ac.Timeout = ptypes.DurationProto(cmd.Timeout)
-	}
 	t, _ := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
 	ar := &repb.ActionResult{
 		ExitCode: int32(res.ExitCode),
@@ -166,10 +153,49 @@ func (e *TestEnv) Set(cmd *command.Command, opt *command.ExecutionOptions, res *
 			OutputUploadCompletedTimestamp: timeToProto(t.Add(9 * time.Millisecond)),
 		},
 	}
-	acDg = digest.TestNewFromMessage(ac)
 	for _, o := range opts {
-		o.Apply(ar, e.Server)
+		if err := o.Apply(ar, e.Server, e.ExecRoot); err != nil {
+			e.t.Fatalf("error applying option %+v: %v", o, err)
+		}
 	}
+
+	root, inputs, _, err := tree.ComputeMerkleTree(cmd.ExecRoot, cmd.InputSpec, int(e.Client.GrpcClient.ChunkMaxSize), e.Client.FileMetadataCache)
+	if err != nil {
+		e.t.Fatalf("error building input tree in fake setup: %v", err)
+		return digest.Empty, digest.Empty
+	}
+	for _, inp := range inputs {
+		bytes, err := inp.FullData()
+		if err != nil {
+			e.t.Fatalf("error getting data from input chunker: %v", err)
+		}
+		e.Server.CAS.Put(bytes)
+	}
+
+	cmdPb := cmd.ToREProto()
+	bytes, err := proto.Marshal(cmdPb)
+	if err != nil {
+		e.t.Fatalf("error inserting command digest blob into CAS %v", err)
+	}
+	e.Server.CAS.Put(bytes)
+
+	cmdDg = digest.TestNewFromMessage(cmdPb)
+	ac := &repb.Action{
+		CommandDigest:   cmdDg.ToProto(),
+		InputRootDigest: root.ToProto(),
+		DoNotCache:      opt.DoNotCache,
+	}
+	if cmd.Timeout > 0 {
+		ac.Timeout = ptypes.DurationProto(cmd.Timeout)
+	}
+	acDg = digest.TestNewFromMessage(ac)
+
+	bytes, err = proto.Marshal(ac)
+	if err != nil {
+		e.t.Fatalf("error inserting action digest blob into CAS %v", err)
+	}
+	e.Server.CAS.Put(bytes)
+
 	e.Server.Exec.adg = acDg
 	e.Server.Exec.ActionResult = ar
 	switch res.Status {
@@ -190,7 +216,28 @@ func (e *TestEnv) Set(cmd *command.Command, opt *command.ExecutionOptions, res *
 }
 
 type option interface {
-	Apply(*repb.ActionResult, *Server)
+	Apply(*repb.ActionResult, *Server, string) error
+}
+
+// InputFile to be made available to the fake action.
+type InputFile struct {
+	Path     string
+	Contents string
+}
+
+// Apply creates a file in the execroot with the given content
+// and also inserts the file blob into CAS.
+func (f *InputFile) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
+	bytes := []byte(f.Contents)
+	if err := os.MkdirAll(filepath.Join(execRoot, filepath.Dir(f.Path)), os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create input dir %v: %v", filepath.Dir(f.Path), err)
+	}
+	err := ioutil.WriteFile(filepath.Join(execRoot, f.Path), nil, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to setup file %v under temp exec root %v: %v", f.Path, execRoot, err)
+	}
+	s.CAS.Put(bytes)
+	return nil
 }
 
 // OutputFile is to be added as an output of the fake action.
@@ -200,49 +247,54 @@ type OutputFile struct {
 }
 
 // Apply puts the file in the fake CAS and the given ActionResult.
-func (f *OutputFile) Apply(ac *repb.ActionResult, s *Server) {
+func (f *OutputFile) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	bytes := []byte(f.Contents)
 	s.Exec.OutputBlobs = append(s.Exec.OutputBlobs, bytes)
 	dg := s.CAS.Put(bytes)
 	ac.OutputFiles = append(ac.OutputFiles, &repb.OutputFile{Path: f.Path, Digest: dg.ToProto()})
+	return nil
 }
 
 // StdOut is to be added as an output of the fake action.
 type StdOut string
 
 // Apply puts the action stdout in the fake CAS and the given ActionResult.
-func (o StdOut) Apply(ac *repb.ActionResult, s *Server) {
+func (o StdOut) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	bytes := []byte(o)
 	s.Exec.OutputBlobs = append(s.Exec.OutputBlobs, bytes)
 	dg := s.CAS.Put(bytes)
 	ac.StdoutDigest = dg.ToProto()
+	return nil
 }
 
 // StdOutRaw is to be added as a raw output of the fake action.
 type StdOutRaw string
 
 // Apply puts the action stdout as raw bytes in the given ActionResult.
-func (o StdOutRaw) Apply(ac *repb.ActionResult, s *Server) {
+func (o StdOutRaw) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	ac.StdoutRaw = []byte(o)
+	return nil
 }
 
 // StdErr is to be added as an output of the fake action.
 type StdErr string
 
 // Apply puts the action stderr in the fake CAS and the given ActionResult.
-func (o StdErr) Apply(ac *repb.ActionResult, s *Server) {
+func (o StdErr) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	bytes := []byte(o)
 	s.Exec.OutputBlobs = append(s.Exec.OutputBlobs, bytes)
 	dg := s.CAS.Put(bytes)
 	ac.StderrDigest = dg.ToProto()
+	return nil
 }
 
 // StdErrRaw is to be added as a raw output of the fake action.
 type StdErrRaw string
 
 // Apply puts the action stderr as raw bytes in the given ActionResult.
-func (o StdErrRaw) Apply(ac *repb.ActionResult, s *Server) {
+func (o StdErrRaw) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	ac.StderrRaw = []byte(o)
+	return nil
 }
 
 // ExecutionCacheHit of true will cause the ActionResult to be returned as a cache hit during
@@ -250,6 +302,7 @@ func (o StdErrRaw) Apply(ac *repb.ActionResult, s *Server) {
 type ExecutionCacheHit bool
 
 // Apply on true will cause the ActionResult to be returned as a cache hit during fake execution.
-func (c ExecutionCacheHit) Apply(ac *repb.ActionResult, s *Server) {
+func (c ExecutionCacheHit) Apply(ac *repb.ActionResult, s *Server, execRoot string) error {
 	s.Exec.Cached = bool(c)
+	return nil
 }

go/pkg/tool/tool_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/command"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/fakes"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestTool_DownloadActionResult(t *testing.T) {
@@ -44,5 +45,50 @@ func TestTool_DownloadActionResult(t *testing.T) {
 		if got != want {
 			t.Fatalf("Incorrect content in downloaded file %v, want %v, got %v", fp, want, got)
 		}
+	}
+}
+
+func TestTool_ShowAction(t *testing.T) {
+	e, cleanup := fakes.NewTestEnv(t)
+	defer cleanup()
+	cmd := &command.Command{
+		Args:     []string{"tool"},
+		ExecRoot: e.ExecRoot,
+		InputSpec: &command.InputSpec{
+			Inputs: []string{
+				"a/b/input.txt",
+			},
+		},
+		OutputFiles: []string{"a/b/out"},
+	}
+
+	opt := command.DefaultExecutionOptions()
+	_, acDg := e.Set(cmd, opt, &command.Result{Status: command.CacheHitResultStatus}, &fakes.OutputFile{Path: "a/b/out", Contents: "output"},
+		fakes.StdOut("stdout"), fakes.StdErr("stderr"), &fakes.InputFile{Path: "a/b/input.txt", Contents: "input"})
+
+	toolClient := &Client{GrpcClient: e.Client.GrpcClient}
+	got, err := toolClient.ShowAction(context.Background(), acDg.String())
+	if err != nil {
+		t.Fatalf("ShowAction(%v) failed: %v", acDg.String(), err)
+	}
+	want := `Command
+========
+Command Digest: 76a608e419da9ed3673f59b8b903f21dbf7cc3178281029151a090cac02d9e4d
+	tool
+
+Inputs
+======
+a: [Directory digest: b2fcb5f1407e2324dd81c4685724cac9228c7142cb34d665dfd3f37a8a18342c]
+a/b/input.txt: [File digest: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/0]
+
+
+Output Files:
+=============
+a/b/out, digest: e0ee8bb50685e05fa0f47ed04203ae953fdfd055f5bd2892ea186504254f8c3a
+
+Output Files From Directories:
+=================`
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("ShowAction(%v) returned diff (-want +got): %v\n\ngot: %v\n\nwant: %v\n", acDg.String(), diff, got, want)
 	}
 }

go/api/command/BUILD.bazel
@@ -12,7 +12,7 @@ proto_library(
 go_proto_library(
     name = "command_go_proto",
     importpath = "github.com/bazelbuild/remote-apis-sdks/go/api/command",
-    proto = ":command_proto",
+    proto = ":cmd_proto",
     visibility = ["//visibility:public"],
 )
 
@@ -21,4 +21,11 @@ go_library(
     embed = [":command_go_proto"],
     importpath = "github.com/bazelbuild/remote-apis-sdks/go/api/command",
     visibility = ["//visibility:public"],
+)
+
+proto_library(
+    name = "cmd_proto",
+    srcs = ["command.proto"],
+    visibility = ["//visibility:public"],
+    deps = ["@com_google_protobuf//:timestamp_proto"],
 )

go/cmd/remotetool/main.go
@@ -28,6 +28,7 @@ type OpType string
 
 const (
 	downloadActionResult OpType = "download_action_result"
+	showAction           OpType = "show_action"
 )
 
 var (
@@ -42,7 +43,7 @@ func validate() {
 	}
 
 	switch OpType(*operation) {
-	case downloadActionResult:
+	case downloadActionResult, showAction:
 		return
 
 	default:
@@ -71,6 +72,13 @@ func main() {
 		if err := c.DownloadActionResult(ctx, *digest, *pathPrefix); err != nil {
 			log.Exitf("error downloading action result for digest %v: %v", *digest, err)
 		}
+
+	case showAction:
+		res, err := c.ShowAction(ctx, *digest)
+		if err != nil {
+			log.Exitf("error fetching action %v: %v", *digest, err)
+		}
+		fmt.Fprintf(os.Stdout, res)
 
 	default:
 		log.Exitf("unsupported operation %v.", *operation)

go/pkg/fakes/cas.go
@@ -3,6 +3,7 @@ package fakes
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -386,8 +387,60 @@ func (f *CAS) BatchReadBlobs(ctx context.Context, req *repb.BatchReadBlobsReques
 }
 
 // GetTree implements the corresponding RE API function.
-func (f *CAS) GetTree(*repb.GetTreeRequest, regrpc.ContentAddressableStorage_GetTreeServer) error {
-	return status.Error(codes.Unimplemented, "test fake does not implement method")
+func (f *CAS) GetTree(req *repb.GetTreeRequest, stream regrpc.ContentAddressableStorage_GetTreeServer) error {
+	rootDigest, err := digest.NewFromProto(req.RootDigest)
+	if err != nil {
+		return fmt.Errorf("unable to parsse root digest %v", req.RootDigest)
+	}
+	blob, ok := f.Get(rootDigest)
+	if !ok {
+		return fmt.Errorf("root digest %v not found", rootDigest)
+	}
+	rootDir := &regrpc.Directory{}
+	proto.Unmarshal(blob, rootDir)
+
+	res := []*regrpc.Directory{rootDir}
+	queue := []*regrpc.Directory{rootDir}
+	for len(queue) > 0 {
+		ele := queue[0]
+		res = append(res, ele)
+		queue = queue[1:]
+
+		for _, inpFile := range ele.GetFiles() {
+			fd, err := digest.NewFromProto(inpFile.GetDigest())
+			if err != nil {
+				return fmt.Errorf("unable to parse file digest %v", inpFile.GetDigest())
+			}
+			blob, ok := f.Get(fd)
+			if !ok {
+				return fmt.Errorf("file digest %v not found", fd)
+			}
+			dir := &regrpc.Directory{}
+			proto.Unmarshal(blob, dir)
+			queue = append(queue, dir)
+			res = append(res, dir)
+		}
+
+		for _, dir := range ele.GetDirectories() {
+			fd, err := digest.NewFromProto(dir.GetDigest())
+			if err != nil {
+				return fmt.Errorf("unable to parse directory digest %v", dir.GetDigest())
+			}
+			blob, ok := f.Get(fd)
+			if !ok {
+				return fmt.Errorf("directory digest %v not found", fd)
+			}
+			directory := &regrpc.Directory{}
+			proto.Unmarshal(blob, directory)
+			queue = append(queue, directory)
+			res = append(res, directory)
+		}
+	}
+
+	resp := &repb.GetTreeResponse{
+		Directories: res,
+	}
+	return stream.Send(resp)
 }
 
 // Write implements the corresponding RE API function.

go/pkg/tool/BUILD.bazel
@@ -9,6 +9,7 @@ go_library(
         "//go/pkg/client:go_default_library",
         "//go/pkg/digest:go_default_library",
         "//go/pkg/filemetadata:go_default_library",
+        "//go/pkg/tree:go_default_library",
         "@com_github_bazelbuild_remote_apis//build/bazel/remote/execution/v2:go_default_library",
         "@com_github_golang_glog//:go_default_library",
     ],
@@ -21,6 +22,6 @@ go_test(
     deps = [
         "//go/pkg/command:go_default_library",
         "//go/pkg/fakes:go_default_library",
-        "//go/pkg/outerr:go_default_library",
+        "@com_github_google_go_cmp//cmp:go_default_library",
     ],
 )

go/pkg/tool/tool.go
@@ -3,13 +3,16 @@
 package tool
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/digest"
 	"github.com/bazelbuild/remote-apis-sdks/go/pkg/filemetadata"
+	"github.com/bazelbuild/remote-apis-sdks/go/pkg/tree"
 
 	rc "github.com/bazelbuild/remote-apis-sdks/go/pkg/client"
 	repb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
@@ -29,20 +32,9 @@ type Client struct {
 // DownloadActionResult downloads the action result of the given action digest
 // if it exists in the RBE cache.
 func (c *Client) DownloadActionResult(ctx context.Context, actionDigest, pathPrefix string) error {
-	acDg, err := digest.NewFromString(actionDigest)
+	resPb, err := c.getActionResult(ctx, actionDigest)
 	if err != nil {
 		return err
-	}
-	d := &repb.Digest{
-		Hash:      acDg.Hash,
-		SizeBytes: acDg.Size,
-	}
-	resPb, err := c.GrpcClient.CheckActionCache(ctx, d)
-	if err != nil {
-		return err
-	}
-	if resPb == nil {
-		return fmt.Errorf("action digest %v not found in cache", d)
 	}
 
 	log.Infof("Downloading action results of %v to %v.", actionDigest, pathPrefix)
@@ -77,4 +69,155 @@ func (c *Client) DownloadActionResult(ctx context.Context, actionDigest, pathPre
 	}
 	log.Infof("Successfully downloaded results of %v to %v.", actionDigest, pathPrefix)
 	return nil
+}
+
+// ShowAction parses and displays an action with its corresponding command.
+func (c *Client) ShowAction(ctx context.Context, actionDigest string) (string, error) {
+	var showActionRes bytes.Buffer
+	resPb, err := c.getActionResult(ctx, actionDigest)
+	if err != nil {
+		return "", err
+	}
+
+	acDg, err := digest.NewFromString(actionDigest)
+	if err != nil {
+		return "", err
+	}
+	actionProto := &repb.Action{}
+	if err := c.GrpcClient.ReadProto(ctx, acDg, actionProto); err != nil {
+		return "", err
+	}
+	showActionRes.WriteString("Command\n========\n")
+	showActionRes.WriteString(fmt.Sprintf("Command Digest: %v\n", actionProto.GetCommandDigest().Hash))
+
+	log.Infof("Reading command from action digest..")
+	commandProto := &repb.Command{}
+	cmdDg, err := digest.NewFromProto(actionProto.GetCommandDigest())
+	if err != nil {
+		return "", err
+	}
+	if err := c.GrpcClient.ReadProto(ctx, cmdDg, commandProto); err != nil {
+		return "", err
+	}
+	cmdStr := strings.Join(commandProto.GetArguments(), " ")
+	showActionRes.WriteString(fmt.Sprintf("\t%v\n", cmdStr))
+
+	log.Infof("Fetching input tree from input root digest..")
+	inpTree, err := c.getInputTree(ctx, actionProto.GetInputRootDigest())
+	if err != nil {
+		return "", err
+	}
+	showActionRes.WriteString("\nInputs\n======\n")
+	showActionRes.WriteString(inpTree)
+
+	log.Infof("Fetching output tree from action result..")
+	outs, err := c.getOutputs(ctx, resPb)
+	if err != nil {
+		return "", err
+	}
+	showActionRes.WriteString("\n\n")
+	showActionRes.WriteString(outs)
+	return showActionRes.String(), nil
+}
+
+func (c *Client) getOutputs(ctx context.Context, actionRes *repb.ActionResult) (string, error) {
+	var res bytes.Buffer
+	res.WriteString("Output Files:\n=============")
+	for _, of := range actionRes.GetOutputFiles() {
+		res.WriteString(fmt.Sprintf("\n%v, digest: %v", of.GetPath(), of.GetDigest().Hash))
+	}
+
+	res.WriteString("\n\nOutput Files From Directories:\n=================")
+	for _, od := range actionRes.GetOutputDirectories() {
+		treeDigest := od.GetTreeDigest()
+		dg, err := digest.NewFromProto(treeDigest)
+		if err != nil {
+			return "", err
+		}
+		outDirTree := &repb.Tree{}
+		if err := c.GrpcClient.ReadProto(ctx, dg, outDirTree); err != nil {
+			return "", err
+		}
+
+		outputs, err := c.flattenTree(ctx, outDirTree)
+		if err != nil {
+			return "", err
+		}
+		res.WriteString("\n")
+		res.WriteString(outputs)
+	}
+	return res.String(), nil
+}
+
+func (c *Client) getInputTree(ctx context.Context, root *repb.Digest) (string, error) {
+	var res bytes.Buffer
+
+	dg, err := digest.NewFromProto(root)
+	if err != nil {
+		return "", err
+	}
+	rootDir := &repb.Directory{}
+	if err := c.GrpcClient.ReadProto(ctx, dg, rootDir); err != nil {
+		return "", fmt.Errorf("error fetching root dir proto for digest %v: %v", dg, err)
+	}
+
+	if len(rootDir.GetDirectories()) < 1 {
+		return "", nil
+	}
+	rootDirNode := rootDir.GetDirectories()[0]
+	res.WriteString(fmt.Sprintf("%v: [Directory digest: %v]", rootDirNode.GetName(), rootDirNode.GetDigest().Hash))
+
+	dirs, err := c.GrpcClient.GetDirectoryTree(ctx, rootDirNode.GetDigest())
+	if err != nil {
+		return "", err
+	}
+	t := &repb.Tree{
+		Root:     rootDir,
+		Children: dirs,
+	}
+	inputs, err := c.flattenTree(ctx, t)
+	if err != nil {
+		return "", err
+	}
+	res.WriteString("\n")
+	res.WriteString(inputs)
+
+	return res.String(), nil
+}
+
+func (c *Client) flattenTree(ctx context.Context, t *repb.Tree) (string, error) {
+	var res bytes.Buffer
+	outputs, err := tree.FlattenTree(t, "")
+	if err != nil {
+		return "", err
+	}
+	for path, output := range outputs {
+		if output.IsEmptyDirectory {
+			res.WriteString(fmt.Sprintf("%v: [Directory digest: %v]\n", path, output.Digest))
+		} else if output.SymlinkTarget != "" {
+			res.WriteString(fmt.Sprintf("%v: [Symlink digest: %v, Symlink Target: %v]\n", path, output.Digest, output.SymlinkTarget))
+		} else {
+			res.WriteString(fmt.Sprintf("%v: [File digest: %v]\n", path, output.Digest))
+		}
+	}
+	return res.String(), nil
+}
+
+func (c *Client) getActionResult(ctx context.Context, actionDigest string) (*repb.ActionResult, error) {
+	acDg, err := digest.NewFromString(actionDigest)
+	if err != nil {
+		return nil, err
+	}
+	d := &repb.Digest{
+		Hash:      acDg.Hash,
+		SizeBytes: acDg.Size,
+	}
+	resPb, err := c.GrpcClient.CheckActionCache(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+	if resPb == nil {
+		return nil, fmt.Errorf("action digest %v not found in cache", d)
+	}
+	return resPb, nil
 }

commit a357a3fe5198442e5f77572d6f5bf7e19d4bc53e

Fixing bug in fake and integration test (#473)
Bug was introduced in #161.
Fake input files were never actually written to disk, like they were supposed to.

go/pkg/fakes/server.go
@@ -288,7 +288,7 @@ func (f *InputFile) apply(ac *repb.ActionResult, s *Server, execRoot string) err
 	if err := os.MkdirAll(filepath.Join(execRoot, filepath.Dir(f.Path)), os.ModePerm); err != nil {
 		return fmt.Errorf("failed to create input dir %v: %v", filepath.Dir(f.Path), err)
 	}
-	err := os.WriteFile(filepath.Join(execRoot, f.Path), nil, 0755)
+	err := os.WriteFile(filepath.Join(execRoot, f.Path), bytes, 0755)
 	if err != nil {
 		return fmt.Errorf("failed to setup file %v under temp exec root %v: %v", f.Path, execRoot, err)
 	}

go/pkg/tool/tool_test.go
@@ -83,8 +83,8 @@ Platform
 
 Inputs
 ======
-[Root directory digest: e23e10be0d14b5b2b1b7af32de78dea554a74df5bb22b31ae6c49583c1a8aa0e/75]
-a/b/input.txt: [File digest: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855/0]
+[Root directory digest: 1b1d5b9e3eb97866805deca901299fcdde98b5747e147f35187e12c16e120186/75]
+a/b/input.txt: [File digest: c96c6d5be8d08a12e7b5cdc1b207fa6b2430974c86803d8891675e76fd992c20/5]
 
 ------------------------------------------------------------------------
 Action Result