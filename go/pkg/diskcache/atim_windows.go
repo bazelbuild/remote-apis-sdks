1046709ff9ad0107e55c3e36ecda80bcb1dcde7e
Ola Rozenfeld <ola@engflow.com>
2024-01-31 17:31:50 -0500

    Local disk cache: part 1

---
+++ go/pkg/diskcache/atim_windows.go
@@ -0,0 +1,18 @@
+// Utility to get the last accessed time on Windows.
+package diskcache
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+// This will return correct values only if `fsutil behavior set disablelastaccess 0` is set.
+// Tracking of last access time is disabled by default on Windows.
+func GetLastAccessTime(path string) (time.Time, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.Unix(0, info.Sys().(*syscall.Win32FileAttributeData).LastAccessTime.Nanoseconds()), nil
+}
